50b36a17161646d4ff88133c99e230b2f2e643d4
MARTINSD <MARTINSD@lab.ecinf.ch>
2018-12-03 13:42:31 +0100

    NavBar Retractable

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Titre de la page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/uikit.min.css" />
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.min.js"></script>
+    </head>
+
+
+    <body class="">
+        
+        <?php require "navbar.php"; ?>
+
+    </body>
---
+++ navbar.php
@@ -0,0 +1,24 @@
+<nav class="uk-navbar uk-navbar-container uk-margin ">
+    <div class="uk-navbar-left">
+
+        <a href="#offcanvas-slide" class="uk-button uk-button-default" uk-navbar-toggle-icon uk-toggle></a>
+
+    </div>
+
+
+</nav>
+
+<div id="offcanvas-slide" uk-offcanvas>
+    <div class="uk-offcanvas-bar">
+
+        <ul class="uk-nav uk-nav-default">
+            <li class="uk-active"><a href="#">EE Matos</a></li>
+            <li class="uk-nav-header">Categorie</li>
+            <li><a href="#">Ecran</a></li>
+            <li><a href="#">Bidule</a></li>
+            <li class="uk-nav-divider"></li>
+            <li><a href="#">Info</a></li>
+        </ul>
+
+    </div>
+</div>
