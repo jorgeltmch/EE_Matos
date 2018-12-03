1d146a665890f265153e9a1e74f3b6c481d9818e
JAUCHW <JAUCHW@lab.ecinf.ch>
2018-12-03 15:34:02 +0100

    Ajout page profile.php

---
+++ profile.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Profile</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/uikit.min.css" />
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.min.js"></script>
+    </head>
+    <body class="">
+      <?php require "navbar.php"; ?>
+
+      </div>
+      <div class="uk-margin-xlarge-left">
+
+        <h1 style="text-decoration: underline">Profile</h1>
+        <img src="./img/homer.jpg" width="230" height="230" style="float: left">
+        <div style="float: left">
+        <h2 class="uk-margin-xlarge-left">Homer Simpson</h2>
+        <div class="uk-margin-medium-left">
+          <p>Nom : SIMPSON</p>
+          <p>Prénom : Homer</p>
+          <p>Adresse : homer.smpsn@eduge.ch</p>
+          <p>Nombre de prêts : 3</p>
+        </div>
+<div class="uk-child-width-expand@s uk-grid uk-position-center-right" style="margin-right: 30%; margin-top: -180px">
+
+  <div class="">
+
+      <h4>Prêt</h4>
+      <ul class="uk-list uk-list-large uk-list-divider">
+          <li>iPad Pro</li>
+          <li>Chaise</li>
+          <li>Imprimante 3D</li>
+      </ul>
+  </div>
+
+  <div>
+      <h4>Début</h4>
+      <ul class="uk-list uk-list-large uk-list-divider">
+          <li>28.11.18</li>
+          <li>12.11.18</li>
+          <li>03.11.18</li>
+      </ul>
+  </div>
+
+  <div>
+      <h4>Fin</h4>
+      <ul class="uk-list uk-list-large uk-list-divider">
+          <li>10.12.18</li>
+          <li>15.11.18</li>
+          <li>05.11.18</li>
+      </ul>
+  </div>
+</div>
+
+</div>
+
+    </body>
+</html>
