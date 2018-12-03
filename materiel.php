e94dd1bd38bf3694fdd42178990fac3ae652ba23
LEITEJ <LEITEJ@lab.ecinf.ch>
2018-12-03 14:56:17 +0100

    Ajout de la navbar

---
+++ materiel.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Matos</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/uikit.min.css" />
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.min.js"></script>
+    </head>
+    <body>
+      <header>
+        <?php  include("navbar.php"); ?>
+      </header>
+      <h1 class=" uk-margin-left uk-heading-bullet uk-heading-line">Ecran AOC 24" LED - C24G1</h1>
+
+
+    </body>
+</html>
--- navbar.php
+++ navbar.php
@@ -1,10 +1,24 @@
 <nav class="uk-navbar uk-navbar-container uk-margin ">
-    <div class="uk-navbar-left">
+    <div class="uk-navbar-left uk-margin-left">
 
         <a href="#offcanvas-slide" class="uk-button uk-button-default" uk-navbar-toggle-icon uk-toggle></a>
+        <div class="uk-navbar-left">
+              <a class="uk-navbar-item uk-logo" href="#">Matos</a>
+        </div>
 
     </div>
 
+    <div class="uk-navbar-right">
+
+        <ul class="uk-navbar-nav">
+          <form class="uk-search uk-search-default uk-margin-right">
+              <span class="uk-search-icon-flip" uk-search-icon></span>
+              <input class="uk-search-input" type="search" placeholder="Search...">
+          </form>
+            <li><button class="uk-button uk-button-secondary uk-margin-right">Login</button></li>
+        </ul>
+        
+    </div>
 
 </nav>
 
