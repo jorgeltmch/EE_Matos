1a4637a487086372216a8b1e7cc7107fd0978d84
MARTINSD <MARTINSD@lab.ecinf.ch>
2018-12-03 13:48:00 +0100

    Ajout de la page ulkit.php Our l'include dans toute les page pour acceder au fonction ulkit

--- index.php
+++ index.php
@@ -4,10 +4,7 @@
     <head>
         <meta charset="utf-8">
         <title>Titre de la page</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/uikit.min.css" />
-        <script src="js/uikit.min.js"></script>
-        <script src="js/uikit-icons.min.js"></script>
+        <?php require "ulkit.php"; ?>
     </head>
 
 
