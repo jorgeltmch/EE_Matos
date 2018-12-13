55eecff96ec8a9844b9cb6f644c068917e671580
LEITEJ <LEITEJ@lab.ecinf.ch>
2018-12-13 07:57:07 +0100

    ajout de la pop-up de reservation

--- materiel.php
+++ materiel.php
@@ -60,7 +60,14 @@
                   <div id="modal-center" class="uk-flex-top" uk-modal>
                       <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
                           <button class="uk-modal-close-default" type="button" uk-close></button>
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                          <form class="uk-form">
+                            <fieldset class="uk-fieldset">
+                            <legend class="uk-legend">Réserver ce produit</legend>
+                            <p>Date de début : <input type="text" data-uk-datepicker="{format:'DD.MM.YYYY'}"></p>
+                            <p>Date de fin : <input type="text" data-uk-datepicker="{format:'DD.MM.YYYY'}"></p>
+                            </fieldset>
+                            <button class="uk-button uk-button-default uk-float-right">Réserver</button>
+                          </form>
                       </div>
                     </div>
 
