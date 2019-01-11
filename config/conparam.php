ffee0ab1afd1dc3fa13c2d0effbabab5a0871d85
LEITEJ <LEITEJ@lab.ecinf.ch>
2019-01-11 07:31:56 +0100

    correction bugs

--- config/conparam.php
+++ config/conparam.php
@@ -3,14 +3,14 @@
 // Le Type de la base de donnée
 define ('EDB_DBTYPE', "mysql");
 // Adresse de l'hôte
-define ('EDB_HOST', "127.0.0.1");
+define ('EDB_HOST', "http://www.ee.ecinf.ch");
 // Numéro de Port
 define ('EDB_PORT', "3306");
 // Nom de la base de donnée
 define ('EDB_DBNAME', "matos");
 // Nom du compte admin de la base de donnée
-define ('EDB_USER', "root");
+define ('EDB_USER', "matosAdmin@localhost");
 // Mot de passe du compte admin
-define ('EDB_PASS', "");
+define ('EDB_PASS', "SuperMatos");
 
 ?>
--- php/fonction.php
+++ php/fonction.php
@@ -3,7 +3,7 @@ require_once '../classe/database.php';
 
 function addProduit($nom, $dateAjout, $dateModif)
  {
-   $sql = 'INSERT INTO categoriesw(nomCategorie, dateAjout, dateModif) VALUES(:nom, :dateAjout, :dateModif)';
+   $sql = 'INSERT INTO categorie(nomCategorie, dateAjout, dateModif) VALUES(:nom, :dateAjout, :dateModif)';
    $req = EDatabase::prepare($sql, array(PDO::ATTR_CURSOR, PDO::CURSOR_SCROLL));
    $req->execute(array(
            ':nom' => $nom,
